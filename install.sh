61ebda6f5bc9d185a9f3edc7ee21ef4925bbc57c
flebedev77 <flebedev99@gmail.com>
2025-06-30 22:49:39 +0930

    Added plugin system to vim

--- install.sh
+++ install.sh
@@ -8,19 +8,25 @@ printf "\nContinue? [y/N]: "
 read CONTINUE
 
 if [[ $CONTINUE == *Y* || $CONTINUE == *y* ]]; then
+  # Clear old neovim configs
   rm -rf ~/.config/nvim
   rm -rf ~/.local/state/nvim
   rm -rf ~/.local/share/nvim
-  cp -v "$SCRIPT_DIR/vim/vimrc" "$HOME/.vimrc"
 
+  # Configure vim
+  cp -v "$SCRIPT_DIR/vim/vimrc" "$HOME/.vimrc"
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  vim -c 'PlugInstall' -c 'qa'
+
+  # Install stuff into the .config directory
   cp -rv "$SCRIPT_DIR"/* "$CONFIG_PATH/"
 
-  rm -rv "$CONFIG_PATH/vim"
-
-  "$CONFIG_PATH/nvim/install.sh" "$SCRIPT_DIR/nvim/mappings.lua"
-
+  # Remove stray directories
   pushd $CONFIG_PATH 2> /dev/null
-  rm -rf install.sh .git .gitmodules pull_from_local.sh README.md
+  rm -rvf install.sh .git .gitmodules pull_from_local.sh README.md vim
   popd 2> /dev/null
+
+  # Install nvchad neovim
+  "$CONFIG_PATH/nvim/install.sh" "$SCRIPT_DIR/nvim/mappings.lua"
 fi
 
